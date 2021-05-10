dcd7c50f5cffd8ec613ac823daa3fe384f36123c
codernaija11 <66638496+codernaija11@users.noreply.github.com>
2021-05-10 23:16:02 +0100

    made chages to routes.js

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -8,7 +8,7 @@ const dataCtrl = require("../controllers/dataControllers");
 // POST request to create a new data
 router.post("/data", dataCtrl.createNewData);
 // GET request to /data to fetch all data
-router.get("/data", dataCtrl.fetchData);
+router.get("/", dataCtrl.fetchData);
 // GET request to /data/id to fetch a single data
 router.get("/data/:id", dataCtrl.fetchSingleData);
 // Put request to /data/:id to update a singledata
